a3fa33e054593d32510090072f045849606f48ab
saifeeeeee <saifurrehmanasim20@gmail.com>
2026-05-17 02:02:03 +0500

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGhPages = process.env.DEPLOY_TARGET === 'gh-pages'
+
 export default defineConfig({
+  base: isGhPages ? '/2dbeamcalculator/' : '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
